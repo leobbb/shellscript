e2b5ebaffb5ecb63e6eb590b0f7f9d3cc0c58538
leobbb <yzx966@qq.com>
2015-12-17 21:00:12 +0800

    test function

--- test.sh
+++ test.sh
@@ -1,1 +1,23 @@
+#!/bin/bash
+
 myName="leo"
+
+function func()
+{
+  if [ ! "" == "$1" ] 
+  then 
+    echo "\$1 is $1" 
+  else 
+    echo "value is null"
+  fi
+}
+
+echo "Start to test" 
+echo "func "
+func 
+
+echo ""
+echo "func haha"
+func haha
+
+echo "Test is end"
